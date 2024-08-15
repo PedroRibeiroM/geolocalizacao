f04db055515c72c3d9ee6d33781174efc03797bb
aannddrree <aannddrree.silva@gmail.com>
2024-08-15 10:38:08 -0300

    v1

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,15 +1,25 @@
-// lib/location_service.dart
-
-
 import 'package:geolocator/geolocator.dart';
+
+/**
+  Verificação de Serviço e Permissão: O código primeiro verifica se 
+  o serviço de localização está habilitado e se o aplicativo tem as 
+  permissões necessárias para acessar a localização.
+  
+  Obtenção da Localização: Se o serviço estiver habilitado e as permissões 
+  concedidas, o código obtém a posição atual do usuário (latitude e longitude).
+ */
+
+
 
 class LocationService {
   Future<Position> getCurrentLocation() async {
+    // Verifica se o serviço de localização está habilitado.
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       throw Exception('Serviço de localização desabilitado.');
     }
 
+    // Verifica se o aplicativo tem permissão para acessar a localização.
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
@@ -22,7 +32,7 @@ class LocationService {
       throw Exception(
           'Permissões de localização permanentemente negadas.');
     }
-
+    // Retorna a posição atual do usuário.
     return await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
   }
